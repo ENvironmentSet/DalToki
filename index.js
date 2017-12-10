31f7ac10630dd8175866452fc948d060236816dc
environmentset <sbshw1@naver.com>
2017-12-10 21:26:06 +0900

    프로그램화 진행중

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-var Runner = require("./lib/Runner");
-var program = JSON.parse(require("fs").readFileSync("./TTeok_dependencies.json","UTF-8"));
+"use strict";
+var path = require("path");
+var fs = require("fs");
+var interpreter_path = process.argv[1]; // 주소 다루는 모듈 하나 만들어야할듯.
+var program_dir_path = process.argv[2];
+var Runner = require(path.join(interpreter_path,"lib","Runner.js"));
+var program = JSON.parse(fs.readFileSync(path.join(program_dir_path,"TTeok_dependencies.json")));
 var VM = Runner();
 
 program.dependencies.map( function loadHeaderFiles (name) {
-    VM.load(name);
+    VM.load(path.join(program_dir_path,name));
 });
 
-VM.load(program.main);
+VM.load(path.join(program_dir_path,program.main));
 VM.execute();
