d9d3363c2cb27bc904d3524be6d10650b4853e7b
environmentset <sbshw1@naver.com>
2017-12-23 04:28:48 +0900

    http 모둘 작업 및 약간의 변경

--- standard_modules/standard_http.js
+++ standard_modules/standard_http.js
@@ -5,8 +5,8 @@ function setBuiltInHttpFunctions (WINDOW,Var,Func,Exception) {
     function createHttpObject (dis) {
         if(dis.constructor !== Var) new Exception("destination is not a variable");
         dis.change([
-            [{ type: 'identifier', value: 'PORT' },{ type: 'identifier', value: 'HEADER' },{ type: 'identifier', value: 'CALLBACK' }],
-            [{ type: 'integer', value: 80 },{ type: 'object', value: undefined },{ type: 'string', value: '' }]]
+            [{ type: 'identifier', value: 'PORT' },{ type: 'identifier', value: 'HEADER' },{ type: 'identifier', value: 'RESULT' }],
+            [{ type: 'integer', value: 80 },{ type: 'object', value: undefined },{ type: 'object', value: undefined }]]
             ,"object");
     }
 }
