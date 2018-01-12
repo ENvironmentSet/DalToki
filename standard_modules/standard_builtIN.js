53263f9080f8ac9bf5831b4d2eb6e40c66f199eb
environmentset <sbshw1@naver.com>
2018-01-13 01:37:59 +0900

    내부에 있는 값들을 대문자로 통일하는 패치

--- standard_modules/standard_builtIN.js
+++ standard_modules/standard_builtIN.js
@@ -1,28 +1,28 @@
 module.exports = setBuiltInFunctions;
 
 function setBuiltInFunctions (WINDOW,Var,Func,Exception) {
-    WINDOW.scope["@PRINT"] = new Func("built-in",print,["src"]);
+    WINDOW.scope["@PRINT"] = new Func("BUILT-IN",print,["src"]);
     function print (variable,_scope,_callback) {
         if(variable.type !== "string") variable = variable.get().toString();
         console.log(variable.get_unwrap());
     }
-    WINDOW.scope["@ADD"] = new Func("built-in",add,["dis","src"]);
+    WINDOW.scope["@ADD"] = new Func("BUILT-IN",add,["dis","src"]);
     function add (dis,src,_scope,_callback) {
         dis = dis.get();
         src = src.get();
         switch (dis.type) {
-            case "integer":
-                if(src.type === "integer") {
+            case "INTEGER":
+                if(src.type === "INTEGER") {
                     dis.add(src);
                 } else throw new RunnerException("Integer addition is available at only integer type");
                 break;
-            case "string":
-                if(src.type === "string") {
+            case "STRING":
+                if(src.type === "STRING") {
                     dis.concat(src);
                 } else throw new RunnerException("String addition is available at only string type");
                 break;
-            case "float":
-                if(src.type === "integer" || src.type === "float") {
+            case "FLOAT":
+                if(src.type === "INTEGER" || src.type === "FLOAT") {
                     dis.add(src);
                 } else throw new RunnerException("Float addition is available at only integer and float type");
                 break;
@@ -30,18 +30,18 @@ function setBuiltInFunctions (WINDOW,Var,Func,Exception) {
                 throw new RunnerException("@ADD function's [DIS] arg's type is must integer,string,float");
         }
     }
-    WINDOW.scope["@SUB"] = new Func("built-in",sub,["dis","src"]);
+    WINDOW.scope["@SUB"] = new Func("BUILT-IN",sub,["dis","src"]);
     function sub (dis,src,_scope,_callback) {
         dis = dis.get();
         src = src.get();
         switch (dis.type) {
-            case "integer":
-                if(src.type === "integer") {
+            case "INTEGER":
+                if(src.type === "INTEGER") {
                     dis.sub(src);
                 } else throw new RunnerException("Integer sub is available at only integer type");
                 break;
-            case "float":
-                if(src.type === "integer" || src.type === "float") {
+            case "FLOAT":
+                if(src.type === "INTEGER" || src.type === "FLOAT") {
                     dis.sub(src);
                 } else throw new RunnerException("Float sub is available at only integer and float type");
                 break;
@@ -49,18 +49,18 @@ function setBuiltInFunctions (WINDOW,Var,Func,Exception) {
                 throw new RunnerException("@SUB function's [DIS] arg's type is must integer,float");
         }
     }
-    WINDOW.scope["@MUL"] = new Func("built-in",mul,["dis","src"]);
+    WINDOW.scope["@MUL"] = new Func("BUILT-IN",mul,["dis","src"]);
     function mul (dis,src,_scope,_callback) {
         dis = dis.get();
         src = src.get();
         switch (dis.type) {
-            case "integer":
-                if(src.type === "integer") {
+            case "INTEGER":
+                if(src.type === "INTEGER") {
                     dis.mul(src);
                 } else throw new RunnerException("Integer sub is available at only integer type");
                 break;
-            case "float":
-                if(src.type === "integer" || src.type === "float") {
+            case "FLOAT":
+                if(src.type === "INTEGER" || src.type === "FLOAT") {
                     dis.mul(src);
                 } else throw new RunnerException("Float sub is available at only integer and float type");
                 break;
@@ -68,11 +68,11 @@ function setBuiltInFunctions (WINDOW,Var,Func,Exception) {
                 throw new RunnerException("@SUB function's [DIS] arg's type is must integer,float");
         }
     }
-    WINDOW.scope["@DEBUG_LOG"] =  new Func("built-in",debug_log,["src"]);
+    WINDOW.scope["@DEBUG_LOG"] =  new Func("BUILT-IN",debug_log,["src"]);
     function debug_log (item,_scope,_callback) {
         console.log(require("util").inspect(item.get_unwrap(),{showHidden : false,depth : null}));
     }
-    WINDOW.scope["@ASSIGNMENT"] = new Func("built-in",assignment,["dis","src"]);
+    WINDOW.scope["@ASSIGNMENT"] = new Func("BUILT-IN",assignment,["dis","src"]);
     function assignment (dis,src) {
         src = src.get();
         dis.change(src.value,src.type);
